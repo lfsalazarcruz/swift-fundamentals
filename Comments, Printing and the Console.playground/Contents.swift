e5eeaa55d7a4402c02ca5bfb91fb3e8778c6e1d3
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-11-21 21:05:43 -0800

    Variables, Comments, Printing, Console

---
+++ Comments, Printing and the Console.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+var monsterHealth = 19
+
+let myName : String = "Fernando"
+
+var myAge : Int = 27
+
+let myDetails = "\(myName), \(myAge)"
+
+// strings, booleans, integers, floating points, double,
+
+let wholeNumbers : Int = 12
+let text : String = "abc"
+let booleans : Bool = true
+let floatingPointNumber : Float = 1.345
+let double : Double = 3.14159265359
+
+print(monsterHealth)
+
+
+
+
+
